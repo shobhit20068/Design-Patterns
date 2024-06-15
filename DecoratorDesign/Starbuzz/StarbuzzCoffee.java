bbf320eb6ed22577751e12269f4798edb2b4e98d
s0n04bu <Shobhit.Nautiyal@walmart.com>
2024-06-15 15:36:37 +0530

    decorator enhanced

--- DecoratorDesign/Starbuzz/StarbuzzCoffee.java
+++ DecoratorDesign/Starbuzz/StarbuzzCoffee.java
@@ -1,17 +1,37 @@
 package DecoratorDesign.Starbuzz;
 
 import DecoratorDesign.Starbuzz.component.Beverage;
+import DecoratorDesign.Starbuzz.concreteComponent.DarkRoast;
+import DecoratorDesign.Starbuzz.concreteComponent.Decaf;
 import DecoratorDesign.Starbuzz.concreteComponent.Espresso;
 import DecoratorDesign.Starbuzz.concreteComponent.HouseBlend;
+import DecoratorDesign.Starbuzz.concreteDecorator.Milk;
 import DecoratorDesign.Starbuzz.concreteDecorator.Mocha;
+import DecoratorDesign.Starbuzz.concreteDecorator.Soy;
+import DecoratorDesign.Starbuzz.concreteDecorator.Whip;
 
 public class StarbuzzCoffee {
     public static void main(String args[]) {
-        Beverage beverage = new Espresso();
-        System.out.println(beverage.getDescription() + " $" + beverage.cost());
+       Beverage beverage = new Espresso();
+		System.out.println(beverage.getDescription() 
+				+ " $" + beverage.cost());
+ 
+		Beverage beverage2 = new DarkRoast();
+		beverage2 = new Mocha(beverage2);
+		beverage2 = new Mocha(beverage2);
+		beverage2 = new Whip(beverage2);
+		System.out.println(beverage2.getDescription() 
+				+ " $" + beverage2.cost());
+ 
+		Beverage beverage3 = new HouseBlend();
+		beverage3 = new Soy(beverage3);
+		beverage3 = new Mocha(beverage3);
+		beverage3 = new Whip(beverage3);
+		System.out.println(beverage3.getDescription() 
+				+ " $" + beverage3.cost());
 
-        Beverage beverage2 = new HouseBlend();
-        beverage2 = new Mocha(beverage2);
-        System.out.println(beverage2.getDescription() + " $"+beverage2.cost());
+        Beverage beverage4 = new Decaf();
+        beverage4 = new Milk(beverage4);
+        System.out.println(beverage4.getDescription() + " $" + beverage4.cost());
     }
 }
